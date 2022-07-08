641cfb038120dd3f764851ba93a8538986b07ed4
halowe <haleylowe0@gmail.com>
2022-07-07 21:58:29 -0700

    fixed app.listen function

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000
 
-app.listen(port. () => console.log(`listening on port ${port}`));
+app.listen(port, () => {
+    return console.log(`listening on port ${port}`);
+});
 
 app.get('/express', (req, res) => {
     res.send({ express: 'YOUR BACKEND IS CONNECTED TO REACT' });
